69eff0f37ca999f864bdb4c933052bf2e13f1ad3
s30485 <s30485@pjwstk.edu.pl>
2025-03-20 13:49:35 +0100

    changed structure of the repo to include the unit tests, added some changes to the program

---
+++ ABPD_HW_02.Tests/Managers/DeviceManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using ABPD_HW_02.Managers;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ABPD_HW_02.Tests.Managers;
+
+[TestSubject(typeof(DeviceManager))]
+public class DeviceManagerTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
--- Interfaces/IPowerNotifier.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ABPD_HW_02;
-
-public interface IPowerNotifier
-{
-    void NotifyLowBattery();
-}
